ccde6a8b3c8ec85bc7995b36de3c33577640f739
jasminechych <jasminechych@gmail.com>
2022-12-27 00:46:16 +0800

    Add sum function

--- src/components/main/cart/Cart.js
+++ src/components/main/cart/Cart.js
@@ -24,22 +24,36 @@ const data = [
 
 export default function Cart () {
 
-  const [amount1, setAmount1] = useState(10)
-  console.log(`頭的amount1: ${amount1}`)
-  const [amount2, setAmount2] = useState(20)
-  console.log(`頭的amount2: ${amount2}`)
+  // 先設假初始值測試用
+  // amount1 綁定 id 1
+  // amount2 綁定 id 2
+  const [amount1, setAmount1] = useState(100)
+  const [amount2, setAmount2] = useState(200)
+  const [total, setTotal] = useState(amount1 + amount2)
+
+  console.log('父amount1： ', amount1)
+  console.log('父amount2： ', amount2)
+  console.log('父total： ',total)
+
 
   return (  
     <div className={style.cart}>
       <h3 className={style.cartTitle}>購物籃</h3>
-      <ProductList data={data} amount1={setAmount1} amount2={setAmount2}/>
+      <ProductList 
+        data={data}
+        setAmount1={setAmount1}
+        setAmount2={setAmount2}
+        amount1={amount1}
+        amount2={amount2}
+        setTotal={setTotal}
+        />
       <CartInfo text="運費" price="免費"/>
-      <CartInfo text="小計" price="0"/>
+      <CartInfo text="小計" price={total}/>
     </div>
   )
 }
 
-function ProductList ({ data, amount1, amount2 }) {
+function ProductList ({ data, setAmount1, setAmount2, amount1, amount2, setTotal }) {
   return (
     <section className={style.productSection}>
       {data.map(datum => 
@@ -50,30 +64,61 @@ function ProductList ({ data, amount1, amount2 }) {
         img={datum.img}
         price={datum.price}
         quantity={datum.quantity}
-        setAmount={datum.id === 1 ? amount1 : amount2}
+        setAmount1={setAmount1}
+        setAmount2={setAmount2}
+        amount1={amount1}
+        amount2={amount2}
+        setTotal={setTotal}
       />)}
     </section>
   )
 }
 
-function Product ({ id, name, img, price, quantity, setAmount }) {
+function Product ({ id, name, img, price, quantity, setAmount1, setAmount2, amount1, amount2, setTotal }) {
   const [count, setCount] = useState(quantity)
 
-  function set () {
-    if(id === 1) {
-      setAmount(100)
+  // 渲染初始資料
+  // data.map(datum => {
+  //   console.log(datum.id, datum.price, datum.quantity)
+  //   if (datum.id === "1") {
+  //     return setAmount1(datum.price * datum.quantity) + setTotal(amount1 + amount2)
+  //   } 
+    
+  //   if (datum.id === "2") {
+  //     return setAmount2(datum.price * datum.quantity) + setTotal(amount1 + amount2)
+  //   }
+  // })
+
+  // set 小計跟加總
+  function set (id, price, content) { 
+    console.log(`id: ${id} click ${content} price: ${price}`)
+      
+    if (id === "1" && content === "plus") {
+      return setAmount1(amount1 + price) + setTotal(amount1 + price + amount2)
+    } 
+    if (id === "1" && content === "minus") {
+      if (count === 0 || amount1 === 0) return
+      return setAmount1(amount1 - price) + setTotal(amount1 - price + amount2)
     }
+
+    if (id === "2" && content === "plus") {
+      return setAmount2(amount2 + price) + setTotal(amount2 + price + amount1)
+    } 
+    if (id === "2" && content === "minus") {
+      if (count === 0 || amount2 === 0) return
+      return setAmount2(amount2 - price) + setTotal(amount2 - price + amount1)
+    } 
   }
-  set ()
 
-
-  function minus () {
-    if (count === 0) return
-    setCount(prev => prev - 1)
-  }
-
-  function plus () {
-    setCount(prev => prev + 1)
+  function handleOnClicked (productId, price, content) {      
+      if (content === "plus") {
+        setCount(prev => prev + 1)
+        set(productId, price, "plus")
+        
+      } else if (content === "minus" && count > 0 ) {
+        setCount(prev => prev - 1)
+        set(productId, price, "minus")
+      }  
   }
 
   return (
@@ -82,15 +127,16 @@ function Product ({ id, name, img, price, quantity, setAmount }) {
       <div className={style.productWrap}>
         <div className={style.productInfo}>
           <div className={style.productName}>{name}</div>
+                                                  {/* 這邊資料應該要連動 */}
           <div className={style.productPrice}>{price * count}</div>
         </div>
         <div className={style.productControlContainer}>
           <span className={style.productControlBtn}>
-            <Minus onClick={minus} />
+            <Minus onClick={() => handleOnClicked(id, price, "minus")} />
           </span>
           <span className={style.productCount}>{count}</span>
           <span className={style.productControlBtn}>
-            <Plus onClick={plus} />
+            <Plus onClick={() => handleOnClicked(id, price, "plus")} />
           </span>
         </div>
       </div>
